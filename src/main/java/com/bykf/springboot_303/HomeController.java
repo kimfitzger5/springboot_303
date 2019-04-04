6168599e85767d91bfe2f19ccb222144997d3e2d
Kim FitzGerald <kimfitzger@gmail.com>
2019-04-03 20:24:28 -0400

    Fixed!

--- src/main/java/com/bykf/springboot_303/HomeController.java
+++ src/main/java/com/bykf/springboot_303/HomeController.java
@@ -22,30 +22,32 @@ public class HomeController {
         model.addAttribute("courses",courseRepository.findAll());
         return "list";
     }
-    @GetMapping ("add")
+    @GetMapping ("/add")
     public String courseForm(Model model){
-        model.addAttribute("courses", courseRepository.findAll());
-        return "list";
+        model.addAttribute("course",new Course());
+        return "courseform";
     }
 
     @PostMapping("/process")
     public String processForm(@Valid Course course,
-                               BindingResult result){
+    BindingResult result){
         if(result.hasErrors()){
             return "courseform";
         }
         courseRepository.save(course);
         return "redirect:/";
     }
+
     @RequestMapping("/detail/{id}")
-    public String showCourse(@PathVariable("id") long id, Model model) {
+    public String showCourse(@PathVariable("id") long id, Model model)
+    {
         model.addAttribute("course", courseRepository.findById(id).get());
         return "show";
-
     }
+
         @RequestMapping("/update/{id}")
-                public String updateCourse(@PathVariable("id") long id,
-        Model model){
+        public String updateCourse(@PathVariable("id") long id,
+            Model model){
             model.addAttribute("course", courseRepository.findById(id).get());
             return"courseform";
     }
